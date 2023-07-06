c53e0f07d4d120f9911b225d3ea19fdd7796d832
Nikunj Gagrani <104256141+nikunjgagrani@users.noreply.github.com>
2023-07-06 08:49:21 +0530

    Update mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect("mongodb://localhost:27017/LoginSignup")
+mongoose.connect("mongodb://127.0.0.1:27017/LoginSignup")
 .then(()=>{
     console.log("mongodb connected");
 })
